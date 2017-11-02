21d1dcf41982b460d138c8eafb94168c94d9236f
Niklas <nb@sbe.de>
2017-11-02 21:24:25 +0100

    dev

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var tpl *template.Template
+
+func init() {
+
+}
 
 func main() {
-    fmt.Printf("hello, world\n")
+	router := httprouter.New()
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+	router.GET("/", view)
+	router.GET("/api", api)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func view(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	tpl = template.Must(template.ParseGlob("template/*.html"))
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func api(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	r.ParseForm()
+
+	fmt.Println(r.Form)
+	fmt.Println(r.Form["sosse1"])
+	fmt.Println(r.Form["sosse2"])
 }
