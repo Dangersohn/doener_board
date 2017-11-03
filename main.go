0a432df7832efcde1390d023f4c74ad8d86955c2
apocs <apocs@nblaptop.intranet>
2017-11-03 15:48:55 +0100

    switch to echo

--- main.go
+++ main.go
@@ -3,32 +3,45 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/labstack/echo"
 )
 
-var tpl *template.Template
+type Template struct {
+	templates *template.Template
+}
 
-func init() {
-
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func main() {
-	router := httprouter.New()
-	router.GET("/", view)
-	router.GET("/api", api)
-	log.Fatal(http.ListenAndServe(":8000", router))
+	t := &Template{
+		templates: template.Must(template.ParseGlob("template/*.html")),
+	}
+
+	e := echo.New()
+	e.Renderer = t
+	e.GET("/", show)
+	e.GET("/api", api)
+	log.Fatal(e.Start(":8000"))
 }
 
-func view(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	tpl = template.Must(template.ParseGlob("template/*.html"))
-	tpl.ExecuteTemplate(w, "index.html", nil)
+func show(c echo.Context) error {
+	return c.Render(http.StatusOK, "index.html", nil)
 }
 
-func api(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	r.ParseForm()
-	fmt.Println(r.Form["salat"])
-	fmt.Println(r.Form)
+func api(c echo.Context) error {
+	sosse := c.QueryParam("sosse")
+	fmt.Println(sosse)
+	return c.String(http.StatusOK, "Soßen: "+sosse)
 }
+
+//func api(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+//	r.ParseForm()
+//	fmt.Println(r.Form["salat"])
+//	fmt.Println(r.Form)
+//}
